ec315824d591fe56fd23b8a82ee7e931b6a83901
Kingsten Banh <kingstenbanh@gmail.com>
2014-12-07 15:02:41 -0800

    increase number size

--- js/app.js
+++ js/app.js
@@ -2,12 +2,12 @@ $(function() {
 
   //Set up instafeed
   var feed = new Instafeed({
-      clientId: 'ceb2c2a3f09b41569a13a6053c3c9380',
+      clientId: '90acb3463d72415c8a6c19db6791da12',
       target: 'instafeed',
       get: 'tagged',
       tagName: 'photographyportfolio',
       links: true,
-      limit: 8,
+      limit: 12,
       sortBy: 'most-recent',
       resolution: 'standard_resolution',
       template: '<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3"><div class="photo-box"><div class="image-wrap"><a href="{{link}}"><img src="{{image}}"></a><div class="likes">{{likes}} Likes</div></div><div class="description">{{caption}}<div class="date">{{model.date}}</div></div></div></div>'
